957b125103997c76109ae3f7d3f57d0056dca121
Nikolajus Karpovas <mwsoftofficial@gmail.com>
2020-03-01 17:58:05 +0100

    Opensource all the things

---
+++ README.md
@@ -0,0 +1,13 @@
+## Superhero Match
+Superhero Match is an open source dating app.
+
+## consumer-superhero-match
+This micro-service is responsible for consuming new match message and storing the data in database and cache. 
+
+## Becoming Contributor
+If you want to become contributor, please send email to this address - `superheromatch@gmail.com`.
+
+## License
+Copyright 2019-2020 MWSOFT
+
+Licensed under the GPLv3: http://www.gnu.org/licenses/gpl-3.0.html
--- internal/config/firebase.go
+++ internal/config/firebase.go
@@ -1,7 +1,20 @@
+/*
+  Copyright (C) 2019 - 2020 MWSOFT
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package config
 
 // Firebase holds all the Firebase related data.
 type Firebase struct {
-	FunctionAddress string `env:"FIREBASE_NEW_MATCH_FUNCTION_ADDRESS" default:"https://us-central1-superheromatch.cloudfunctions.net/newMatch"`
+	FunctionAddress string `env:"FIREBASE_NEW_MATCH_FUNCTION_ADDRESS"`
 	ContentType     string `env:"FIREBASE_NEW_MATCH_CONTENT_TYPE" default:"application/json"`
 }
